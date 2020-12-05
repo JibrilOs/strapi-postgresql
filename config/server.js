980b187767a13ef861819a621d7a6b7784b0fa74
unknown <webxite23@gmail.com>
2020-12-05 13:09:55 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '83d3785e159ef6d352afa3984c65ca47'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b874ac2e-4d5e-4c84-99b4-218f4842ac6f'
+};
